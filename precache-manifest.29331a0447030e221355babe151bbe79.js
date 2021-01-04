904ba8066c644f18eacbec6d13fbdb8eb9648f2f
ganjgo <ganjgog@gmail.com>
2021-01-04 22:04:57 +0100

    Updates

---
+++ precache-manifest.29331a0447030e221355babe151bbe79.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e52f3e08feb1571f2e019d627df58b7",
+    "url": "/sabinaschool/index.html"
+  },
+  {
+    "revision": "745e33110254350f23f2",
+    "url": "/sabinaschool/static/css/main.f66eed1e.chunk.css"
+  },
+  {
+    "revision": "305d81d7eeb3b206e745",
+    "url": "/sabinaschool/static/js/2.1dacd0a3.chunk.js"
+  },
+  {
+    "revision": "ff36efe99bbdc543e19fafd98fdef32c",
+    "url": "/sabinaschool/static/js/2.1dacd0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745e33110254350f23f2",
+    "url": "/sabinaschool/static/js/main.2168a1eb.chunk.js"
+  },
+  {
+    "revision": "fe3e50a89ab0b88a30a0",
+    "url": "/sabinaschool/static/js/runtime-main.bc402ea5.js"
+  },
+  {
+    "revision": "12293ea8928381ab015f1ac646a7ae90",
+    "url": "/sabinaschool/static/media/1.12293ea8.jpg"
+  },
+  {
+    "revision": "4bd74acdd21c027db219491698e0e4c3",
+    "url": "/sabinaschool/static/media/2.4bd74acd.jpg"
+  },
+  {
+    "revision": "e2aaccfb92db0ed6a385a89bf537cb1f",
+    "url": "/sabinaschool/static/media/3.e2aaccfb.jpg"
+  },
+  {
+    "revision": "99b2f786b304681b83a80d410273a057",
+    "url": "/sabinaschool/static/media/4.99b2f786.jpg"
+  },
+  {
+    "revision": "50b61aaf3c283369df2a43e242a58ca1",
+    "url": "/sabinaschool/static/media/5.50b61aaf.jpg"
+  },
+  {
+    "revision": "f0370cf921ff03e7e8abae5ce8181ef5",
+    "url": "/sabinaschool/static/media/6.f0370cf9.jpg"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/sabinaschool/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/sabinaschool/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "653e1c6f71aaf8ae95d558e4b2e1c500",
+    "url": "/sabinaschool/static/media/claude.653e1c6f.jpg"
+  },
+  {
+    "revision": "86cbb7289c3d569963bdb3f4123f8f28",
+    "url": "/sabinaschool/static/media/colors.86cbb728.gif"
+  },
+  {
+    "revision": "b001b10b1b512bc983a69bc909ed9e64",
+    "url": "/sabinaschool/static/media/composition.b001b10b.jpg"
+  },
+  {
+    "revision": "ef149cf92af0aad5163a436d70a765b2",
+    "url": "/sabinaschool/static/media/drums.ef149cf9.jpg"
+  },
+  {
+    "revision": "f083284da376915c34d105e29d393710",
+    "url": "/sabinaschool/static/media/mujo_01.f083284d.jpg"
+  },
+  {
+    "revision": "cc4ad19a14983fc50e34b56445565971",
+    "url": "/sabinaschool/static/media/mujo_02.cc4ad19a.jpg"
+  },
+  {
+    "revision": "d198236dcafab9a19a8d3dd4df664d74",
+    "url": "/sabinaschool/static/media/mujo_03.d198236d.jpg"
+  },
+  {
+    "revision": "5f40840958bf02da6285cf6eb341b11c",
+    "url": "/sabinaschool/static/media/newdude.5f408409.jpg"
+  },
+  {
+    "revision": "a11c78a75d8c96d59c0c6c803ca2d540",
+    "url": "/sabinaschool/static/media/old1.a11c78a7.jpg"
+  },
+  {
+    "revision": "7f721c00805165a5c4cb0162876f930c",
+    "url": "/sabinaschool/static/media/old11.7f721c00.jpg"
+  },
+  {
+    "revision": "1b1dbbbcdc16a6cebff93240573343c8",
+    "url": "/sabinaschool/static/media/old3.1b1dbbbc.jpg"
+  },
+  {
+    "revision": "d44e10b1d870346d24e0323304caed12",
+    "url": "/sabinaschool/static/media/piano.d44e10b1.jpg"
+  },
+  {
+    "revision": "9b682efef9f451257402756c29aadef8",
+    "url": "/sabinaschool/static/media/piano1.9b682efe.jpg"
+  },
+  {
+    "revision": "d89ced7a1d35a799c59303181509193d",
+    "url": "/sabinaschool/static/media/piano2.d89ced7a.jpg"
+  },
+  {
+    "revision": "6fcc973af813fb4faa63faa537bf2e23",
+    "url": "/sabinaschool/static/media/piano3.6fcc973a.jpg"
+  },
+  {
+    "revision": "b99f2d144fad3097ff9f7b3659270d3e",
+    "url": "/sabinaschool/static/media/piano4.b99f2d14.jpg"
+  },
+  {
+    "revision": "cd57cefb04a2fd79fc65cfdddf6f9bf1",
+    "url": "/sabinaschool/static/media/piano5.cd57cefb.jpg"
+  },
+  {
+    "revision": "6cfe842e1869d30d051c637a21f061f4",
+    "url": "/sabinaschool/static/media/piano6.6cfe842e.jpg"
+  },
+  {
+    "revision": "12541bab1c0b988a5d3b62a7a0dc5400",
+    "url": "/sabinaschool/static/media/piano__01.12541bab.jpg"
+  },
+  {
+    "revision": "293e283063397d722b70492257d7fe85",
+    "url": "/sabinaschool/static/media/piano__02.293e2830.jpg"
+  },
+  {
+    "revision": "99c68bafe1c4bc9c40d9af2796a76176",
+    "url": "/sabinaschool/static/media/piano__03.99c68baf.jpg"
+  },
+  {
+    "revision": "9bfe30d029b4d9be59854cae0386c9ac",
+    "url": "/sabinaschool/static/media/piano__04.9bfe30d0.jpg"
+  },
+  {
+    "revision": "8d8682fa485830cff2502f38cecd5db9",
+    "url": "/sabinaschool/static/media/piano__05.8d8682fa.jpg"
+  },
+  {
+    "revision": "81fb78ca0bd9a853513a6f807f7f79e0",
+    "url": "/sabinaschool/static/media/piano__06.81fb78ca.jpg"
+  },
+  {
+    "revision": "019b32905e39c5c7996dbc81cbcea525",
+    "url": "/sabinaschool/static/media/quote1.019b3290.jpg"
+  },
+  {
+    "revision": "2a6d3ddce430992b7572ed75b9e051d0",
+    "url": "/sabinaschool/static/media/quote2.2a6d3ddc.jpg"
+  },
+  {
+    "revision": "ab987a09ceef8926ceca54b883595cb4",
+    "url": "/sabinaschool/static/media/quote3.ab987a09.jpg"
+  },
+  {
+    "revision": "2b1a0ae5e11460553658a66ff9b7f461",
+    "url": "/sabinaschool/static/media/quote4.2b1a0ae5.jpg"
+  },
+  {
+    "revision": "ffcae65bb6d43a0237c9723ee468b9b7",
+    "url": "/sabinaschool/static/media/sabina.ffcae65b.jpg"
+  },
+  {
+    "revision": "012480c6643e63e77d8a067537f19f29",
+    "url": "/sabinaschool/static/media/slika1.012480c6.jpg"
+  },
+  {
+    "revision": "ee525eb8b8586efc9aacf1b7316dbb6d",
+    "url": "/sabinaschool/static/media/slika2.ee525eb8.jpg"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/sabinaschool/static/media/up-arrow-circle.d6c4e78d.svg"
+  }
+]);
